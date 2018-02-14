672b9a0aae3506eb1e7287f14d381afbee59b4eb
Jorge Barroso <jorge_barroso_11@hotmail.com>
2018-02-14 23:18:40 +0000

    DESede encryption

--- ITManager/DESedeEncryptManager.cpp
+++ ITManager/DESedeEncryptManager.cpp
@@ -2,7 +2,7 @@
 #include "DESedeEncryptManager.h"
 
 
-Encryption::DESedeEncryptManager::DESedeEncryptManager(char const* key_data, unsigned char const* salt, const uint16_t nrounds)
+Encryption::DESedeEncryptManager::DESedeEncryptManager(const char key_data[24], unsigned char const* salt, const uint16_t nrounds)
 {
 	if (strlen(key_data) < 24)
 		throw new std::length_error("Your key should be at least 24 characters long");
@@ -13,7 +13,7 @@ Encryption::DESedeEncryptManager::DESedeEncryptManager(char const* key_data, uns
 	DES_cblock cKey3 = { key_data[2], key_data[5], key_data[8], key_data[11], key_data[14], key_data[17], key_data[20], key_data[23] };
 	if ((DES_set_key_checked(&cKey1, this->SchKey1) || DES_set_key_checked(&cKey2, this->SchKey2) || DES_set_key_checked(&cKey3, this->SchKey3)) == -2)
 	{
-		throw new std::bad_exception("");
+		throw new DesWeakKeyException("");
 	}
 		
 }
@@ -21,23 +21,33 @@ Encryption::DESedeEncryptManager::DESedeEncryptManager(char const* key_data, uns
 
 Encryption::DESedeEncryptManager::~DESedeEncryptManager(){}
 
-std::string Encryption::DESedeEncryptManager::encrypt(const char *)
+std::string Encryption::DESedeEncryptManager::encrypt(const char * key)
 {
 	std::random_device rand;
-	std::mt19937_64(rand());
+	std::mt19937_64 mt(rand());
 
 	std::uniform_int_distribution<int> dist(1, 256);
 
-	DES_cblock iv = (DES_cblock)malloc(8);
+	char cIv[8];
 	for (uint16_t i = 0; i < 8; ++i)
 	{
-
+		cIv[i] = dist(mt);
 	}
 
-	return std::string();
+	DES_cblock iv = { cIv[0], cIv[1], cIv[2], cIv[3], cIv[4], cIv[5], cIv[6], cIv[7] };
+
+	DES_set_odd_parity(&iv);
+
+	const uint16_t length = sizeof(&key);
+	unsigned char ciphertext[length];
+
+	DES_ede3_cbc_encrypt((unsigned char*)key, ciphertext, length, this->SchKey1, this->SchKey2, this->SchKey3, &iv, DES_ENCRYPT);
+
+	return Base64::encode(ciphertext, length);
 }
 
 std::string Encryption::DESedeEncryptManager::decrypt(const char *)
 {
+
 	return std::string();
 }
--- ITManager/DESedeEncryptManager.h
+++ ITManager/DESedeEncryptManager.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "EncryptManager.h"
+#include "DesWeakKeyException.h"
 #include "openssl/des.h"
+#include "Base64.h"
 #include <random>
 #include <stdexcept>
 
@@ -10,10 +12,10 @@ namespace Encryption
 		public EncryptManager
 	{
 	public:
-		DESedeEncryptManager(char const* key_data, unsigned char const* salt, const uint16_t nrounds);
+		DESedeEncryptManager(const char key_data[24], unsigned char const* salt, const uint16_t nrounds);
 		~DESedeEncryptManager();
-		std::string encrypt(const char *);
-		std::string decrypt(const char *);
+		std::string encrypt(const char * key);
+		std::string decrypt(const char *ciphertext);
 	private:
 		DES_key_schedule *SchKey1, *SchKey2, *SchKey3;
 	};
