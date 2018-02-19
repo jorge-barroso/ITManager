533483c3025206dab617a01271b77a77a11f6077
Jorge <jorge_barroso_11@hotmail.com>
2018-02-19 16:16:21 +0000

    Added static config options

--- ITManager/ConfigOptions.cpp
+++ ITManager/ConfigOptions.cpp
@@ -2,4 +2,9 @@
 #include "ConfigOptions.h"
 
 
-const std::string ConfigOptions::GITHUB_USERNAME = "";
+const std::string ConfigOptions::GITHUB_USERNAME = "github.username";
+const std::string ConfigOptions::GTITHUB_PASSWORD = "github.password";
+const std::string ConfigOptions::ENCRYPT_KEY = "encrypt.key";
+const std::string ConfigOptions::ENCRYPT_SALT = "encrypt.salt";
+const std::string ConfigOptions::SSH_KEY_FILE_PUBLIC = "ssh.key_file.public";
+const std::string ConfigOptions::SS_KEY_FILE_PRIVATE = "ssh.key_file.private";
--- ITManager/ConfigOptions.h
+++ ITManager/ConfigOptions.h
@@ -4,5 +4,10 @@ const static class ConfigOptions
 {
 public:
 	const static std::string GITHUB_USERNAME;
+	const static std::string GTITHUB_PASSWORD;
+	const static std::string ENCRYPT_KEY;
+	const static std::string ENCRYPT_SALT;
+	const static std::string SSH_KEY_FILE_PUBLIC;
+	const static std::string SS_KEY_FILE_PRIVATE;
 };
 
